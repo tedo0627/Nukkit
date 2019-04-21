c9c999a40d803a858bf0fc0a4fd97625f0bb0be1
hiroki19990625 <hiroki19990625@gmail.com>
2019-04-22 00:08:08 +0900

    Add Entity Identifiers Settings

--- src/main/java/cn/nukkit/network/protocol/AvailableEntityIdentifiersPacket.java
+++ src/main/java/cn/nukkit/network/protocol/AvailableEntityIdentifiersPacket.java
@@ -1,27 +1,42 @@
 package cn.nukkit.network.protocol;
 
 import cn.nukkit.Nukkit;
+import cn.nukkit.Server;
+import cn.nukkit.nbt.NBTIO;
+import cn.nukkit.nbt.tag.CompoundTag;
+import cn.nukkit.nbt.tag.ListTag;
 import com.google.common.io.ByteStreams;
 import lombok.ToString;
 
 import java.io.IOException;
+import java.nio.ByteOrder;
 
-@ToString(exclude = {"tag"})
+@ToString()
 public class AvailableEntityIdentifiersPacket extends DataPacket {
     public static final byte NETWORK_ID = ProtocolInfo.AVAILABLE_ENTITY_IDENTIFIERS_PACKET;
 
-    private static final byte[] TAG;
+    private static CompoundTag entityIdentifiers;
 
     static {
         try {
-            //noinspection UnstableApiUsage
-            TAG = ByteStreams.toByteArray(Nukkit.class.getClassLoader().getResourceAsStream("entity_identifiers.dat"));
+            byte[] data = ByteStreams.toByteArray(Nukkit.class.getClassLoader().getResourceAsStream("entity_identifiers.dat"));
+            entityIdentifiers = NBTIO.read(data, ByteOrder.LITTLE_ENDIAN, true);
         } catch (NullPointerException | IOException e) {
             throw new AssertionError("Unable to load entity_identifiers_net.dat");
         }
     }
 
-    public byte[] tag = TAG;
+    public static void registerIdentifier(int id, String name) {
+        ListTag<CompoundTag> list = entityIdentifiers.getList("idlist", CompoundTag.class);
+        CompoundTag data = new CompoundTag();
+        data.putBoolean("hasspawnegg", true);
+        data.putBoolean("experimental", false);
+        data.putBoolean("summonable", true);
+        data.putString("id", name);
+        data.putString("bid", ":");
+        data.putInt("rid", id);
+        list.add(data);
+    }
 
     @Override
     public byte pid() {
@@ -30,12 +45,20 @@ public class AvailableEntityIdentifiersPacket extends DataPacket {
 
     @Override
     public void decode() {
-        this.tag = this.get();
+        try {
+            entityIdentifiers = NBTIO.read(this.get(), ByteOrder.LITTLE_ENDIAN, true);
+        } catch (IOException e) {
+            Server.getInstance().getLogger().error("", e);
+        }
     }
 
     @Override
     public void encode() {
         this.reset();
-        this.put(this.tag);
+        try {
+            this.put(NBTIO.write(entityIdentifiers, ByteOrder.LITTLE_ENDIAN, true));
+        } catch (IOException e) {
+            Server.getInstance().getLogger().error("", e);
+        }
     }
 }

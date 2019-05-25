c57e8c7b5d64d9fd6182b52e864b114acc4ebf21
tedo0627 <tedosw1mpr1ver@gmail.com>
2019-05-25 19:11:00 +0900

    ほかのダメージもnodelayになってたのを修正

--- src/main/java/cn/nukkit/entity/EntityLiving.java
+++ src/main/java/cn/nukkit/entity/EntityLiving.java
@@ -140,8 +140,11 @@ public abstract class EntityLiving extends Entity implements EntityDamageable {
             pk.event = this.getHealth() <= 0 ? EntityEventPacket.DEATH_ANIMATION : EntityEventPacket.HURT_ANIMATION;
             Server.broadcastPacket(this.hasSpawned.values(), pk);
 
-            //this.attackTime = 10;
-            this.attackTime = 0;
+            this.attackTime = 10;
+            DamageCause cause = source.getCause();
+            if (cause == DamageCause.ENTITY_ATTACK || cause == DamageCause.PROJECTILE) {
+                this.attackTime = 0;
+            }
 
             return true;
         } else {
